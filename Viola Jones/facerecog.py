c0fc4ed8213587c193b4aa17937bb0c1fd54576f
Tanmay Bhatnagar <tanmaybhatnagar84976@gmail.com>
2018-10-14 23:39:46 +0530

    Face recognition complete

--- Viola Jones/facerecog.py
+++ Viola Jones/facerecog.py
@@ -16,4 +16,17 @@ def detect(gray, frame):
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.1 , 3)
         for ( ex , ey , ew , eh ) in eyes:
             cv2.rectangle(roi_color , (ex,ey) , (ex+ew , ey+eh) , ( 0 , 255 , 0 ) , 2)
+    return frame
+
+#Face recognition with webcam
+#0 - Webcam of device
+#1 - External webcam
+video_capture = cv2.VideoCapture(0)
+while True:
+    _, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
+    cv2.imshow('Video' , canvas)
+    if cv2.waitKey(1) & OxFF == ord('q'):
+        break
             
