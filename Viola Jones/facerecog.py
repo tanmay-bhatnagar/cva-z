a966b753339a2cf0d947e8eb2e80a7d3d2a26925
Tanmay Bhatnagar <tanmaybhatnagar84976@gmail.com>
2018-10-14 23:17:45 +0530

    Setup eye detection and completed detect fn

--- Viola Jones/facerecog.py
+++ Viola Jones/facerecog.py
@@ -3,11 +3,17 @@ import cv2
 
 #Loading Cascades
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_caseace = cv2.CascadeClassifier('haarcascade_eye.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 #Gray - grayscale image
 #frame - original image
 def detect(gray, frame):
     faces = face_cascade.detectMultiScale(gray , 1.3 , 5)
     for ( x , y , w , h ) in faces : 
-        cv2.rectangle(frame, (x,y) , (x+w , y+h) , (255,0,0) , 2)
+        cv2.rectangle(frame, (x,y) , (x+w , y+h) , ( 255 , 0 , 0 ) , 2)
+        roi_gray = gray[y:y+h , x:x+w]
+        roi_color = frame[y:y+h , x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1 , 3)
+        for ( ex , ey , ew , eh ) in eyes:
+            cv2.rectangle(roi_color , (ex,ey) , (ex+ew , ey+eh) , ( 0 , 255 , 0 ) , 2)
+            
